2455a49dd9a523789185a2611c41802b00276107
Nitish Khurana <nitish.khurana@siemens-healthineers.com>
2025-09-16 21:12:38 +0530

    Added retain folder settings

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@ import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TF
 
     interface MoveFilesPluginSettings {
             moveMdFile: boolean;
+            retainFolderStructure: boolean;
     }
 
     const DEFAULT_SETTINGS: MoveFilesPluginSettings = {
             moveMdFile: false,
+            retainFolderStructure: true,
     };
 
     
@@ -59,6 +61,16 @@ export default class MoveFilesPlugin extends Plugin {
                     this.plugin.settings.moveMdFile = value;
                     await this.plugin.saveSettings();
                     }));
+
+                new Setting(containerEl)
+                .setName('Retain folder structure')
+                .setDesc('If enabled, the new folder will be created in the same directory as the original markdown file. If disabled, the new folder will be created in the root directory of the vault.')
+                .addToggle(toggle => toggle
+                    .setValue(this.plugin.settings.retainFolderStructure)
+                    .onChange(async (value) => {
+                    this.plugin.settings.retainFolderStructure = value;
+                    await this.plugin.saveSettings();
+                    }));
             }
             }(this.app, this));
 	}
@@ -88,8 +100,13 @@ export default class MoveFilesPlugin extends Plugin {
             new Notice('No linked files found in the markdown file.');
             return;
         }
-
-        const targetFolderName = `${file.basename} files`;
+        
+        var existingFolderPath = file.parent?.path;
+        if(!this.settings.retainFolderStructure)
+        {
+            existingFolderPath = "";
+        }
+        const targetFolderName = `${existingFolderPath}/${file.basename} files`;
 		const folderExists = this.app.vault.getAbstractFileByPath(targetFolderName);
 		if (!(folderExists instanceof TFolder) )
 		{
